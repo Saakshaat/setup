567ee9e8c510d40ba8393521baed3d829fa09d8a
Saakshaat <saakshaat2001@gmail.com>
2022-08-15 18:59:10 +0530

    finalize shell setup scripts

--- scripts/shell/theme.sh
+++ scripts/shell/theme.sh
@@ -1,7 +1,26 @@
-#!/bin/bash
+#!/bin/env zsh
 
-echo "#####################"
-echo "Theming ZSH with Pure"
-echo "#####################"
+echo "###########"
+echo "Theming OMZ"
+echo "###########"
 
-omz theme set zhann
+read -d '' THEMES <<EOF
+ZSH_THEME_RANDOM_CANDIDATES=(
+  "robbyrussell"
+  "zhann"
+  "clean"
+  "sorin"
+  "gozilla"
+  "arrow"
+  "kolo"
+)
+EOF
+
+# append to beginnging of file
+echo -e "$THEMES\n$(cat ~/.zshrc)" > ~/.zshrc
+
+# search and replace default theme to random
+sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="random"/g' ~/.zshrc
+
+# activate theme
+source ~/.zshrc
