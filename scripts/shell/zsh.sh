9c53ad1d2a842deed0f7375c1c2dd6c716f6f11a
Saakshaat <saakshaat2001@gmail.com>
2022-08-15 17:24:47 +0530

    fix omz setup

--- scripts/shell/zsh.sh
+++ scripts/shell/zsh.sh
@@ -7,6 +7,6 @@ echo "####################"
 brew install zsh
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+`ZSH=~/oh-my-zsh sh install.sh`
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
